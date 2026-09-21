c99e7a9a17a19e7ae126ec5c9f627629c5deecb3
ZXDEHOPA <zedehopaalt@Gmail.com>
2026-09-22 01:00:38 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- Ride_a_Pet.lua
+++ Ride_a_Pet.lua
@@ -87,7 +87,7 @@ local LuckMode = "One Time"
 local LOAD_WAIT = 1.5
 local AFTER_EGG_WAIT = 0.5
 
-local WAYPOINT_WAIT = 0.5
+local WAYPOINT_WAIT = 0.75
 local NEAR_FENCE_WAIT = 10
 
 local WALK_DISTANCE = 8
