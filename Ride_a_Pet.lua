cd38fd8cda8ca9d6e57a3c8c49989e2335cb26a1
ZXDEHOPA <zedehopaalt@Gmail.com>
2026-09-26 22:11:36 +0800

    Update Ride_a_Pet.lua

--- Ride_a_Pet.lua
+++ Ride_a_Pet.lua
@@ -61,7 +61,7 @@ local SelectedPlaceEggs = {}
 local LOAD_WAIT = 1
 local AFTER_EGG_WAIT = 0.5
 local WAYPOINT_WAIT = 1
-local NEAR_BASEPLATE_WAIT = 10
+local NEAR_BASEPLATE_WAIT = 4
 local WALK_DISTANCE = 8
 
 local FallbackEggs = {
